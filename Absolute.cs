ce69177e6475461a955a6b72d8119ec9fca6f097
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 18:38:04 +0300

    First Algorithm

---
+++ Absolute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class Absolute
+    {
+        public static int Abs(int n)
+        {
+            int value;
+            if (n < 0) value = -1 * n;
+            else value = n;
+            return value;
+        }
+    }
+}
---
+++ TekmiCiftmi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class TekmiCiftmi
+    {
+        public static bool IsEven(int n)
+        {
+            if(n % 2 == 1) return true;
+
+            return false;
+        }
+    }
+}
