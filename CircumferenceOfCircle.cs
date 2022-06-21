55c2df1ac48fe8c73fc8a568eaa093d361a7d6d6
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 19:17:59 +0300

    Sixth Algorithm

---
+++ CircumferenceOfCircle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class CircumferenceOfCircle
+    {
+        public static double CircleCM(double r)
+        {
+            const double pi = 3.14;
+            return 2 * pi * r;
+        }
+        public static double CircleAl(double r)
+        {
+            const double pi = 3.14;
+            return pi * r * r;
+        }
+    }
+}
