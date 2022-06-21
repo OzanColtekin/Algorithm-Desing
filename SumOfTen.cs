428786b2f73888e7e40973f15243372be1b46058
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 19:13:34 +0300

    Fifth Algorithm

---
+++ SumOfTen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class SumOfTen
+    {
+        public static int SumOfTenNumber()
+        {
+            int result = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                result++;   
+            }
+            return result;
+        }
+        public static int SumOfEvenTen()
+        {
+            int result = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                if(i %2 == 0) result++;
+            }
+            return result;
+        }
+
+        public static int SumOfOddTen()
+        {
+            int result = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0) result++;
+            }
+            return result;
+        }
+    }
+}
