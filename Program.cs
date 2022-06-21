1699d6c6b759da3dfe5fa1075a6ec85c14eb036a
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 19:08:15 +0300

    Fourth Algorithm

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Algoritma_Tasarımı;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-Console.WriteLine("Hello, World!");
-
-PrimeNumber primeNumber = new PrimeNumber();
-primeNumber.main();
---
+++ SumDigits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class SumDigits
+    {
+        public static int SumOfDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                sum += digit;
+                n = n / 10;
+            }
+            return sum;
+        }
+
+    }
+}
