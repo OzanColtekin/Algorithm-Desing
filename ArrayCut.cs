bae6735a45db2323b7ae76d8b18a9f03d37b230d
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 19:36:21 +0300

    Nineth Algorithm

---
+++ ArrayCut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class ArrayCut
+    {
+        public static int[] EvenNumber(int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0 ) 
+                    result[i] = arr[i];
+            }
+            return result;
+        }}
+    }
+}
