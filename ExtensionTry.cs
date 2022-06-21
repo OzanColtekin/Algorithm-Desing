1dd193550ad1900d03eb6ce2ee23b86420eddc8f
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-22 01:10:02 +0300

    Don't forget extension

---
+++ ExtensionTry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    public static class StringExtension
+    {
+        public static int StringCount(this string text)
+        {
+            return text.Split(" ").Length;
+        }
+    }
+    internal class ExtensionTry
+    {
+        public void Main()
+        {
+            string words = "Hello world with sm1le";
+            Console.WriteLine(words.StringCount());
+        }
+        
+    }
+}
