6f404775f856aa027caa5d05faaaeb886273ba8a
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 19:25:01 +0300

    Seventh Algortihm

---
+++ ArrayEnbEnk.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class ArrayEnbEnk
+    {
+        public static int Enb(int[] arr)
+        {
+            int value = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if(arr[i] > value) value = arr[i];
+            }
+            return value;
+        }
+        public static int Enk(int[] arr)
+        {
+            int value = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < value) value = arr[i];
+            }
+            return value;
+        }
+    }
+}
