3ee5a00b1c2cf1ce0a651148e6f7a69103e3f83a
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 20:02:40 +0300

    Eleventh Algorithm

---
+++ PrimeFactors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class PrimeFactors
+    {
+        public static ArrayList Primes(int x)
+        {
+            ArrayList result = new ArrayList();
+            int i = 2;
+            while(x > 1)
+            {
+                if (x % i == 0)
+                {
+                    x = x / i;
+                    result.Add(i);
+                }
+                else
+                {
+                    i++;
+                }
+                    
+            }
+            return result;
+        }
+        
+    }
+}
