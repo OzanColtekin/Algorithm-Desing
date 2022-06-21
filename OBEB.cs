c97b1b6c6c8f40c43b6eee1dbb9267f034a70afd
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 20:46:27 +0300

    Thirteenth Algorithm

---
+++ OBEB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class OBEB
+    {
+        public static int Obeb(int number1, int number2)
+        {
+            int result = 1;
+            while(number1 !=1 && number2 != 1)
+            {
+                int bol = 2;
+                for(int i = 1; i <= (number1 > number2 ? number1 : number2); i++)
+                {
+                    if (number1 % bol == 0 && number2 % bol == 0)
+                    {
+                        result *= bol;
+                        number2 /= bol;
+                        number1 /= bol;
+                    }
+                    if(number1 % bol == 0) number1 /= bol;
+                    if (number2 % bol == 0) number2 /= bol;
+                    else bol++;
+                    
+                }
+            }   
+            return result;
+        }
+        public void main()
+        {
+            Console.WriteLine(Obeb(18, 24));
+        }
+    }
+}
--- OKEK.cs
+++ OKEK.cs
@@ -14,18 +14,22 @@ namespace Algoritma_Tasarımı
             while(number1 !=1 && number2 != 1)
             {
                 int bol = 2;
-                for(int i = 0; i <= (number1>number2 ? number1 : number2); i++)
+                for(int i = 1; i <= (number1>number2 ? number1 : number2); i++)
                 {
                     if (number1 % bol == 0 || number2 % bol == 0)
                     {
                         s *= bol;
                         if (number1 % bol == 0) number1 /= bol;
-                        else number2 /= bol;
+                        if (number2 % bol == 0) number2 /= bol;
                     }
                     else bol++;
                 }
             }
             return s;
+        }
+        public void main()
+        {
+            Console.WriteLine(Okek(15, 20));
         }
     }
 }
