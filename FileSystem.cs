ae255287835d85300121bbd5537701923c0bc5dd
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-22 00:48:46 +0300

    Don't wanna write anything

---
+++ FileSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class FileSystem
+    {
+        public void Reader()
+        {
+            try
+            {
+
+                string filePath = (string)Directory.GetCurrentDirectory() + "\\data.txt";
+                using StreamReader streamReader = new(filePath);
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Writer()
+        {
+            try
+            {
+                string filePath = (string)Directory.GetCurrentDirectory() + "\\write.txt";
+                using StreamWriter streamWriter = new(filePath);
+                streamWriter.WriteLine("TEST");
+                streamWriter.WriteLine("WRITE");
+            }
+            catch(Exception er)
+            {
+                Console.WriteLine(er.Message);
+            }
+        }
+    }
+}
