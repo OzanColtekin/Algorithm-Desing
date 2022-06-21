2fa49184c75543a2480f62734e8f6b811d93afd1
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 19:03:41 +0300

    Third Algorithm

---
+++ PrimeNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class PrimeNumber
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n < 1) return false;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
