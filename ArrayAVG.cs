3f1e9c166ef92d8ce78d510a4184476c4dc1dc6f
OzanColtekin <ozan.coltekin@outlook.com>
2022-06-21 19:28:10 +0300

    Eigth Algorithm

---
+++ ArrayAVG.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class ArrayAVG
+    {
+        public static double Avg(int[] arr)
+        {
+            return arr.Sum() / arr.Length;
+        }
+    }
+}
