b94723a4fec6a4f332f32acd951cd34762c04fe5
OzanColtekin <ozan.coltekin@outlook.com>
2022-07-01 16:05:51 +0300

    Finally added

---
+++ CompareTheTriples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class CompareTheTriples
+    {
+        public List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            List<int> arr = new List<int>();
+            int alice = 0;
+            int bob = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    alice += 1;
+                }
+                else if (a[i] < b[i])
+                {
+                    bob += 1;
+                }
+            }
+            arr.Add(alice);
+            arr.Add(bob);
+            return arr;
+        }
+    }
+}
---
+++ PlusMinus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Tasarımı
+{
+    internal class PlusMinus
+    {
+        public void plusMinus(List<int> arr)
+        {
+            float pCount = 0, nCount = 0, zCount = 0;
+            foreach (int i in arr)
+            {
+                if (i > 0) pCount += 1;
+                else if (i < 0) nCount += 1;
+                else zCount += 1;
+            }
+            Console.WriteLine(pCount / arr.Count);
+            Console.WriteLine(nCount / arr.Count);
+            Console.WriteLine(zCount / arr.Count);
+        }
+    }
+}
